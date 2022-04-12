0fcfcdf136799894aa9e7224f07f683c810a454c
Pacifist <desmondmorales92@gmail.com>
2022-04-12 23:03:54 +0300

    reworking isNaN

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 // Task #1
 
 let numOrStr = prompt('input number or string');
-
-if (numOrStr === null && numOrStr.trim() === "" && isNaN(+numOrStr)) {
-    console.log("Введите данные")
-}   else {
-    console.log("OK!")
-}
+//
+// if (numOrStr === null && numOrStr.trim() === "" && isNaN(+numOrStr)) {
+//     console.log("Введите данные")
+// }   else {
+//     console.log("OK!")
+// }
 
 // Task #2
 
 switch (numOrStr) {
-    case null:
-        console.log('вы отменили')
-    break;
+    case (null):
+        console.log('вы отменили');
+        break;
 
-    case (numOrStr.trim() === ''):
+    case "":
         console.log('Empty String');
-    break;
+        break;
 
-    case (isNaN( +numOrStr )):
-        console.log(' number is Ba_NaN')
-    break;
-
-    default:
-        console.log('OK!')
-    break;
+    default: {
+        switch (isNaN(+numOrStr)) {
+            case (true):
+                console.log(' number is Ba_NaN');
+                break;
+            default:
+                console.log('OK!');
+                break;
+        }
+    }
 }
